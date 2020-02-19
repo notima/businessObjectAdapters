69ce13db095947c386238c0598d774277528b86c
Daniel Tamm <daniel@notima.se>
2020-02-19 14:02:18 +0100

    Read credentials from fortnoxClients.xml

--- fortnoxAdapter/src/test/java/org/notima/businessobjects/adapter/fortnox/junit/FortnoxAdapterTestBase.java
+++ fortnoxAdapter/src/test/java/org/notima/businessobjects/adapter/fortnox/junit/FortnoxAdapterTestBase.java
@@ -3,8 +3,10 @@ package org.notima.businessobjects.adapter.fortnox.junit;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 
-import org.apache.commons.configuration.CompositeConfiguration;
 import org.junit.Before;
+import org.notima.api.fortnox.FortnoxUtil;
+import org.notima.api.fortnox.clients.FortnoxClientInfo;
+import org.notima.api.fortnox.clients.FortnoxClientList;
 import org.notima.businessobjects.adapter.fortnox.FortnoxAdapter;
 import org.notima.generic.ifacebusinessobjects.BusinessObjectFactory;
 import org.slf4j.Logger;
@@ -20,12 +22,18 @@ public class FortnoxAdapterTestBase {
 	@Before
 	public void setUp() throws Exception {
 		
-		CompositeConfiguration config = new CompositeConfiguration();
-		URL configUrl = ClassLoader.getSystemResource("test-config3.xml");
-		config.addConfiguration(new org.apache.commons.configuration.XMLConfiguration(configUrl));
+		URL configUrl = ClassLoader.getSystemResource("fortnoxClients.xml");
 		
-		String clientSecret = config.getString("clientSecret");
-		String accessToken = config.getString("accessToken");
+		if (configUrl==null) {
+			throw new Exception("No fortnoxClients.xml found");
+		}
+
+		FortnoxClientList clientList = FortnoxUtil.readFortnoxClientListFromFile(configUrl.getFile());
+		
+		FortnoxClientInfo info = clientList.getFirstClient();
+		
+		String clientSecret = info.getClientSecret();
+		String accessToken = info.getAccessToken();
 		
 		if (clientSecret==null || clientSecret.trim().length()==0) {
 			throw new Exception("Client secret must be supplied.");
