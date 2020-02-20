0fbf395961e984b33a67a19454684c98e5c46070
Daniel Tamm <daniel@notima.se>
2020-02-20 15:02:46 +0100

    Fix bug when amount is negative.
    Remove VAT-lines if vatAmount is zero.

--- fortnoxAdapter/src/main/java/org/notima/businessobjects/adapter/fortnox/FortnoxConverter.java
+++ fortnoxAdapter/src/main/java/org/notima/businessobjects/adapter/fortnox/FortnoxConverter.java
@@ -16,23 +16,35 @@ import org.notima.generic.businessobjects.Invoice;
 import org.notima.generic.businessobjects.Payment;
 import org.notima.generic.businessobjects.PaymentWriteOff;
 
-public class FortnoxConverter extends BasicBusinessObjectConverter {
+/**
+ * Converts Fortnox entities to/from Business Objects entities.
+ * 
+ * @author Daniel Tamm
+ *
+ */
+public class FortnoxConverter extends BasicBusinessObjectConverter<Object, org.notima.api.fortnox.entities3.Invoice> {
 
+	/**
+	 * Converts a Fortnox Invoice to a Business Objects Invoice
+	 * 
+	 */
 	@Override
-	public Invoice fromNativeInvoice(Object src) throws Exception {
+	public Invoice<org.notima.api.fortnox.entities3.Invoice> fromNativeInvoice(Object src) throws Exception {
 		return FortnoxAdapter.convert((org.notima.api.fortnox.entities3.Invoice)src);
 	}
 
 	/**
 	 * Creates a single transaction voucher with a vat amount. 
 	 * 
-	 * @param creditAcct
-	 * @param debitAcct
-	 * @param vatAcct
-	 * @param totalAmount
-	 * @param vatAmount
-	 * @param description
-	 * @return
+	 * @param voucherSeries		The voucher series to use
+	 * @param acctDate			The accounting date
+	 * @param creditAcct		The account to be credited.
+	 * @param debitAcct			The account to be debited.
+	 * @param vatAcct			Vat amount (if any)
+	 * @param totalAmount		The total amount of the transaction
+	 * @param vatAmount			The VAT amount.
+	 * @param description		The description for the voucher text.
+	 * @return	A Fortnox Voucher
 	 */
 	public Voucher createSingleCostWithVatTransactionVoucher(
 			String voucherSeries,
@@ -58,31 +70,47 @@ public class FortnoxConverter extends BasicBusinessObjectConverter {
 		
 		VoucherRow r = new VoucherRow();
 		r.setAccount(Integer.parseInt(creditAcct));
-		r.setCredit(totalAmount);
+		if (totalAmount>0) {
+			r.setCredit(totalAmount);
+		} else {
+			r.setDebit(-totalAmount);
+		}
 		result.addVoucherRow(r);
 		r = new VoucherRow();
 		r.setAccount(Integer.parseInt(debitAcct));
-		r.setDebit(totalAmount - vatAmount);
+		if (totalAmount>0) {
+			r.setDebit(totalAmount - vatAmount);
+		} else {
+			r.setCredit(-totalAmount + vatAmount);
+		}
 		result.addVoucherRow(r);
-		r = new VoucherRow();
-		r.setAccount(Integer.parseInt(vatAcct));
-		r.setDebit(vatAmount);
-		result.addVoucherRow(r);
+		
+		if (vatAmount!=0) {
+			r = new VoucherRow();
+			r.setAccount(Integer.parseInt(vatAcct));
+			if (vatAmount>0)
+				r.setDebit(vatAmount);
+			else 
+				r.setCredit(-vatAmount);
+			
+			result.addVoucherRow(r);
+		}
 		
 		return result;
 		
 	}
 	
 	/**
-	 * Creates a single transaction voucher.
+	 * Creates a single transaction voucher with two lines (debet / credit)
 	 * 
-	 * @param voucherSeries
-	 * @param creditAcct
-	 * @param debitAcct
-	 * @param acctDate
-	 * @param amount
-	 * @param description
-	 * @return
+	 * @param voucherSeries		The voucher series to use
+	 * @param acctDate			The accounting date
+	 * @param creditAcct		The account to be credited.
+	 * @param debitAcct			The account to be debited.
+	 * @param amount		The total amount of the transaction
+	 * @param description		The description for the voucher text.
+	 * 
+	 * @return	A Fortnox Voucher.
 	 */
 	public Voucher createSingleTransactionVoucher(
 			String voucherSeries,
@@ -123,7 +151,7 @@ public class FortnoxConverter extends BasicBusinessObjectConverter {
 	 * @return	A Fortnox equivalent of a payment.
 	 * @throws	Exception if the payment is incomplete.
 	 */
-	public static InvoicePayment toFortnoxPayment(Payment src) throws Exception {
+	public static InvoicePayment toFortnoxPayment(Payment<?> src) throws Exception {
 		
 		if (src==null) return null;
 		
@@ -169,7 +197,12 @@ public class FortnoxConverter extends BasicBusinessObjectConverter {
 		return dst;
 	}
 	
-	
+	/**
+	 * Creates a Fortnox Write off
+	 * 
+	 * @param src		A Business Objects writeoff.
+	 * @return	A Fortnox writeoff.
+	 */
 	public static WriteOff toFortnoxWriteOff(PaymentWriteOff src) {
 		
 		WriteOff dst = new WriteOff();
