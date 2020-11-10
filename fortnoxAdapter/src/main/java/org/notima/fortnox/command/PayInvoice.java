f7947447f9173aaaab9257ec61a8a3e1181a96d3
Daniel Tamm <daniel@notima.se>
2020-11-10 21:12:57 +0100

    Add command to pay customer invoice.

---
+++ fortnoxAdapter/src/main/java/org/notima/fortnox/command/PayInvoice.java
@@ -0,0 +1,160 @@
+package org.notima.fortnox.command;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.api.console.Session;
+import org.notima.api.fortnox.FortnoxClient3;
+import org.notima.api.fortnox.FortnoxException;
+import org.notima.api.fortnox.entities3.Invoice;
+import org.notima.api.fortnox.entities3.InvoicePayment;
+import org.notima.api.fortnox.entities3.ModeOfPayment;
+import org.notima.fortnox.command.table.InvoiceHeaderTable;
+import org.notima.fortnox.command.table.InvoiceLineTable;
+import org.notima.generic.ifacebusinessobjects.BusinessObjectFactory;
+
+@Command(scope = "fortnox", name = "pay-fortnox-invoice", description = "Pays a specific invoice")
+@Service
+public class PayInvoice extends FortnoxCommand implements Action {
+
+	@SuppressWarnings("rawtypes")
+	@Reference
+	private List<BusinessObjectFactory> bofs;
+	
+	@Reference 
+	Session sess;
+
+	@Option(name = "--paydate", description = "Use this date as paydate, default is today. (format yyyy-mm-dd)", required = false, multiValued = false)
+	private String payDateStr;
+
+	@Option(name = "--amount", description = "Pay using this amount. Default is open amount", required = false, multiValued = false)
+	private Double amount;
+	
+	@Option(name = "--no-confirm", description = "Don't confirm anything. Default is to confirm", required = false, multiValued = false)
+	private boolean noConfirm = false;
+	
+	@Option(name = "--no-bookkeep-payment", description = "Don't bookkeep payment.", required = false, multiValued = false)
+	private boolean noBookkeepPayment = false;
+	
+	
+	@Argument(index = 0, name = "orgNo", description ="The orgno of the client", required = true, multiValued = false)
+	private String orgNo = "";
+
+	@Argument(index = 1, name = "invoiceNo", description ="The invoice no", required = true, multiValued = false)
+	private String invoiceNo;
+
+	@Argument(index = 2, name = "modeOfPayment", description ="The mode of payment", required = true, multiValued = false)
+	private String modeOfPayment;
+	
+	@Override
+	public Object execute() throws Exception {
+		
+		FortnoxClient3 fc = getFortnoxClient(bofs, orgNo);
+
+		Date payDate = null;
+		if (payDateStr==null) {
+			payDate = Calendar.getInstance().getTime();
+		} else {
+			try {
+				payDate = FortnoxClient3.s_dfmt.parse(payDateStr);
+			} catch (Exception e) {
+				sess.getConsole().println("Invalid date: " + payDateStr);
+			}
+		}
+		
+		Invoice invoice = fc.getInvoice(invoiceNo);
+
+		if (invoice==null) {
+			sess.getConsole().println("Invoice " + invoiceNo + " not found.");
+			return null;
+		}
+		
+		if (!invoice.isBooked()) {
+			String reply = noConfirm ? "y" : sess.readLine("The invoice is not booked. Do you want to book it? (y/n) ", null);
+			if (reply.equalsIgnoreCase("y")) {
+				
+				try {
+					fc.bookkeepInvoice(invoiceNo);
+				} catch (Exception e) {
+					String msg = null;
+					if (e instanceof FortnoxException) {
+						msg = ((FortnoxException)e).getMessage();
+					} else {
+						msg = e.getMessage();
+					}
+					sess.getConsole().println("Booking failed: " + msg);
+					return null;
+				}
+				
+			} else {
+				sess.getConsole().println("Payment cancelled");
+				return null;
+			}
+		}
+
+		// Check open amount
+		Double openAmt = invoice.getBalance();
+		
+		// Get mode of payment
+		ModeOfPayment mp = null;
+		try {
+			mp = new ModeOfPayment(fc.getModeOfPayment(modeOfPayment));
+		} catch (Exception e) {
+			String msg = null;
+			if (e instanceof FortnoxException) {
+				msg = ((FortnoxException)e).getMessage();
+			} else {
+				msg = e.getMessage();
+			}
+			sess.getConsole().println("Invalid mode of payment ["+ modeOfPayment +"] : " + msg);
+			return null;
+		}
+
+		// Confirm payment
+		String reply = noConfirm ? "y" : sess.readLine("Do you want to pay invoice " + invoiceNo + " with amt " + openAmt + " using account " + mp.getAccountNumber() + ": (y/n) ", null);
+
+		if (reply.equalsIgnoreCase("y")) {
+			
+			try {
+				
+			} catch (Exception e) {
+				String msg = null;
+				if (e instanceof FortnoxException) {
+					msg = ((FortnoxException)e).getMessage();
+				} else {
+					msg = e.getMessage();
+				}
+				sess.getConsole().println("Payment failed: " + msg);
+				return null;
+			}
+			
+		} else {
+			sess.getConsole().println("Payment cancelled");
+			return null;
+		}
+		
+		InvoicePayment pmt = fc.payCustomerInvoice(
+				Integer.parseInt(invoiceNo), 
+				modeOfPayment, 
+				payDate, 
+				openAmt, 
+				null, !noBookkeepPayment);
+		
+		InvoiceHeaderTable iht = new InvoiceHeaderTable(invoice);
+		InvoiceLineTable ilt = new InvoiceLineTable(invoice, null);
+		
+		iht.print(sess.getConsole());
+		ilt.print(sess.getConsole());
+		
+		return null;
+	}
+	
+	
+}
