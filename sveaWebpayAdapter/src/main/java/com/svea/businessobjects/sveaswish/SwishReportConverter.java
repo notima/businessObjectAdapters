64fa3ed704079aacd5004fa1c1042bb092158790
Daniel Tamm <daniel@notima.se>
2021-11-11 15:30:11 +0100

    Include dependency to swish4j.

--- sveaWebpayAdapter/src/main/java/com/svea/businessobjects/sveaswish/SwishReportConverter.java
+++ sveaWebpayAdapter/src/main/java/com/svea/businessobjects/sveaswish/SwishReportConverter.java
@@ -4,18 +4,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import org.notima.swish.reports.SettlementReport;
+import org.notima.swish.reports.SettlementReportParser;
+import org.notima.swish.reports.SettlementReportRow;
+import org.notima.util.LocalDateUtils;
 
 import com.svea.webpay.common.conv.JsonUtil;
 import com.svea.webpay.common.reconciliation.PaymentReport;
 import com.svea.webpay.common.reconciliation.PaymentReportDetail;
 import com.svea.webpay.common.reconciliation.PaymentReportGroup;
-
-import org.notima.swish.reports.SettlementReport;
-import org.notima.swish.reports.SettlementReportParser;
-import org.notima.swish.reports.SettlementReportRow;
 
 public class SwishReportConverter {
 
@@ -44,24 +46,53 @@ public class SwishReportConverter {
     public PaymentReport convert(SettlementReport swishReport, Date fromDate, Date toDate) {
         PaymentReport paymentReport = new PaymentReport();
         PaymentReportGroup group = new PaymentReportGroup();
+        group.setCurrency("SEK");
+        // TODO: The reconciliation date must be derived from the rows
+        group.setReconciliationDate(fromDate);
+        // TODO: This must be done in another way
+        if (swishReport.getRows()!=null && swishReport.getRows().size()>0) {
+        	group.setPaymentTypeReference(swishReport.getRows().get(0).getRecipientNumber());
+        }
         group.setPaymentType(PAYMENT_TYPE_NAME);
         List<PaymentReportDetail> rows = convertRows(swishReport.getRows(), fromDate, toDate);
-        group.setPaymentReportDetail(rows);
+        for (PaymentReportDetail detail : rows) {
+        	group.addDetail(detail);
+        }
         paymentReport.addPaymentReportGroup(group);
         return paymentReport;
     }
 
     private List<PaymentReportDetail> convertRows(List<SettlementReportRow> rows, Date fromDate, Date toDate) {
         List<PaymentReportDetail> details = new ArrayList<PaymentReportDetail>();
+        LocalDate from = LocalDateUtils.asLocalDate(fromDate);
+        LocalDate until = LocalDateUtils.asLocalDate(toDate);
+        LocalDate transactionDate;
         for(SettlementReportRow row : rows) {
-            long rowTimeStamp = row.getTransactionDate().getTime();
-            if(rowTimeStamp >= getStartOfDay(fromDate).getTime() && rowTimeStamp <= getEndOfDay(toDate).getTime()){
+            transactionDate = LocalDateUtils.asLocalDate(row.getTransactionDate());
+            if(includeAfter(transactionDate, from) && 
+            	includeBefore(transactionDate, until)) {
                 details.add(convertToPaymentReportDetail(row));
             }
         }
         return details;
     }
 
+    private boolean includeAfter(LocalDate actualDate, LocalDate afterDate) {
+    	if (actualDate==null || afterDate==null) return true;
+    	if (actualDate.isEqual(afterDate) || actualDate.isAfter(afterDate)) 
+    		return true;
+    	return false;
+    }
+    
+    private boolean includeBefore(LocalDate actualDate, LocalDate beforeDate) {
+    	if (actualDate==null || beforeDate==null) return true;
+    	if (actualDate.isEqual(beforeDate) || actualDate.isBefore(beforeDate))
+    		return true;
+    	return false;
+    	
+    	
+    }
+    
     private PaymentReportDetail convertToPaymentReportDetail(SettlementReportRow row) {
         PaymentReportDetail detail = new PaymentReportDetail();
         detail.setCustomerId(row.getAmount() > 0 ? row.getSenderNumber() : row.getRecipientNumber());
@@ -74,19 +105,4 @@ public class SwishReportConverter {
         return detail;
     }
 
-    private Date getStartOfDay(Date date) {
-        Date result = new Date(date.getTime());
-        result.setHours(0);
-        result.setMinutes(0);
-        result.setSeconds(0);
-        return result;
-    }
-    
-    private Date getEndOfDay(Date date) {
-        Date result = new Date(date.getTime());
-        result.setHours(23);
-        result.setMinutes(59);
-        result.setSeconds(59);
-        return result;
-    }
 }
