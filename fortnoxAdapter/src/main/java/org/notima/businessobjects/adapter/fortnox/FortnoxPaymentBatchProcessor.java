ba163f6cbf174ff29ab70166cecaa321d2fc1b7d
Daniel Tamm <daniel@notima.se>
2021-11-15 14:41:28 +0100

    Work on FortnoxPaymentBatchProcessor

--- fortnoxAdapter/src/main/java/org/notima/businessobjects/adapter/fortnox/FortnoxPaymentBatchProcessor.java
+++ fortnoxAdapter/src/main/java/org/notima/businessobjects/adapter/fortnox/FortnoxPaymentBatchProcessor.java
@@ -3,11 +3,17 @@ package org.notima.businessobjects.adapter.fortnox;
 import java.io.PrintStream;
 import java.util.Properties;
 
+import org.notima.generic.businessobjects.BasicPaymentBatchProcessor;
 import org.notima.generic.businessobjects.PaymentBatch;
 import org.notima.generic.businessobjects.PaymentBatchProcessResult;
-import org.notima.generic.ifacebusinessobjects.PaymentBatchProcessor;
 
-public class FortnoxPaymentBatchProcessor implements PaymentBatchProcessor {
+/**
+ * Processes a payment batch
+ * 
+ * @author Daniel Tamm
+ *
+ */
+public class FortnoxPaymentBatchProcessor extends BasicPaymentBatchProcessor {
 
 	private FortnoxAdapter fortnoxAdapter;
 	
@@ -27,20 +33,16 @@ public class FortnoxPaymentBatchProcessor implements PaymentBatchProcessor {
 
 	@Override
 	public PaymentBatchProcessResult processPaymentBatch(PaymentBatch report, Properties props) throws Exception {
-		// TODO Auto-generated method stub
+
+		setOptionsFromProperties(props);
+		
 		return null;
 	}
 
 	@Override
 	public String getSystemName() {
-		// TODO Auto-generated method stub
-		return null;
+		return FortnoxAdapter.SYSTEMNAME;
 	}
 
-	@Override
-	public void setOutput(PrintStream os) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
