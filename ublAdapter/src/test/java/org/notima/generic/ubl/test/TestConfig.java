0a48723b2653c8b9b8ca74d05788ddc754861241
Daniel Tamm <daniel@notima.se>
2024-01-30 15:55:13 +0100

    Adjust test code for TestUBL21Converter.

--- ublAdapter/src/test/java/org/notima/generic/ubl/test/TestConfig.java
+++ ublAdapter/src/test/java/org/notima/generic/ubl/test/TestConfig.java
@@ -4,7 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URL;
 
-import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
 
 import org.notima.generic.businessobjects.Invoice;
 
@@ -12,15 +13,23 @@ public class TestConfig {
 
 	public static String srcFile1 = "sample-bo-invoice.xml";
 	
-	public static Invoice sampleBoInvoice;
+	public static Invoice<?> sampleBoInvoice;
 	
-	public static void loadConfig() {
+	public static void loadConfig() throws Exception {
 
 		try {
 			URL url = ClassLoader.getSystemResource(srcFile1);
+			if (url==null) {
+				throw new Exception(srcFile1 + " not found. The src/test/resources folder needs to be in classpath when running tests.");
+			}
 			FileReader reader = new FileReader(url.getFile());
 			
-			sampleBoInvoice = (Invoice)JAXB.unmarshal(reader, Invoice.class);
+			sampleBoInvoice = new Invoice<Object>();
+			JAXBContext ctx = JAXBContext.newInstance(Invoice.class);
+			
+			Unmarshaller unmarshaller = ctx.createUnmarshaller();
+			
+			sampleBoInvoice = (Invoice<?>)unmarshaller.unmarshal(reader);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- ublAdapter/src/test/java/org/notima/generic/ubl/test/TestUBL21Converter.java
+++ ublAdapter/src/test/java/org/notima/generic/ubl/test/TestUBL21Converter.java
@@ -1,5 +1,7 @@
 package org.notima.generic.ubl.test;
 
+import static org.junit.Assert.fail;
+
 import java.io.File;
 
 import oasis.names.specification.ubl.schema.xsd.invoice_21.InvoiceType;
@@ -14,15 +16,20 @@ public class TestUBL21Converter {
 	@Test
 	public void testConvert() {
 		
-		TestConfig.loadConfig();
-		
-		if (TestConfig.sampleBoInvoice!=null) {
-
-			InvoiceType result = UBL21Converter.convert(TestConfig.sampleBoInvoice);
-			result = UBL21Converter.addPaymentMeansBankgiro(result, "51234567", result.getIDValue(), "The Fictive Company AB");
+		try {
+			TestConfig.loadConfig();
 			
-			UBL21Writer.invoice().write(result, new File("target/test-invoice.xml"));
-			
+			if (TestConfig.sampleBoInvoice!=null) {
+	
+				InvoiceType result = UBL21Converter.convert(TestConfig.sampleBoInvoice);
+				result = UBL21Converter.addPaymentMeansBankgiro(result, "51234567", result.getIDValue(), "The Fictive Company AB");
+				
+				UBL21Writer.invoice().write(result, new File("target/test-invoice.xml"));
+				
+			}
+		} catch (Exception ee) {
+			fail(ee.getMessage());
+			ee.printStackTrace();
 		}
 		
 	}
