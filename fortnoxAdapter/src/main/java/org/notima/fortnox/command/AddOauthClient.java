2637b43d2ebc8675be7090d41ce285c9786f12a7
Daniel Tamm <daniel@notima.se>
2024-05-30 09:08:09 +0200

    Work in progress AddOauthClient.

--- fortnoxAdapter/src/main/java/org/notima/fortnox/command/AddOauthClient.java
+++ fortnoxAdapter/src/main/java/org/notima/fortnox/command/AddOauthClient.java
@@ -7,8 +7,11 @@ import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.apache.karaf.shell.api.console.Session;
+import org.notima.api.fortnox.Fortnox4jCLI;
 import org.notima.api.fortnox.FortnoxClient3;
 import org.notima.api.fortnox.clients.FortnoxClientManager;
+import org.notima.businessobjects.adapter.fortnox.FortnoxAdapter;
+import org.notima.businessobjects.adapter.tools.FactorySelector;
 
 @Command(scope = _FortnoxCommandNames.SCOPE, name = _FortnoxCommandNames.AddOauthClient, description = "Add an oauth client to Fortnox integration")
 @Service
@@ -28,8 +31,27 @@ public class AddOauthClient extends FortnoxCommand implements Action {
 	
     private FortnoxClient3		fortnoxClient;
     private FortnoxClientManager	clientManager;
+    private Fortnox4jCLI		creator;
     
     private boolean				existingClient = false;
+
+	@Override
+	public Object execute() throws Exception {
+
+		initVariables();
+		printFortnoxClient();
+		if (!checkParameters()) {
+			return null;
+		}
+
+		if (existingClient) {
+			sess.getConsole().println("Client already exists");
+		}
+		
+		createClient();
+		
+		return null;
+	}
     
     private void initVariables() throws Exception {
 
@@ -37,10 +59,31 @@ public class AddOauthClient extends FortnoxCommand implements Action {
     	if (fortnoxClient!=null) {
     		existingClient = true;
     	}
+    	if (bf==null) {
+    		FactorySelector selector = new FactorySelector(bofs);
+    		bf = (FortnoxAdapter)selector.getFirstFactoryFor(FortnoxAdapter.SYSTEMNAME);
+    	}
     	clientManager = bf.getClientManager();
     	
     }
 
+    private boolean checkParameters() {
+    	
+    	if (!hasClientId()) {
+    		sess.getConsole().println("No clientID defined.");
+    		sess.getConsole().println("Use: property-set -p FortnoxProperties defaultClientId [clientId]");
+    		return false;
+    	}
+    	
+    	if (!hasClientSecret()) {
+    		sess.getConsole().println("No client secret defined.");
+    		sess.getConsole().println("Use: property-set -p FortnoxProperties defaultClientSecret [clientSecret]");
+    		return false;
+    	}
+    	
+    	return true;
+    }
+    
     private boolean hasClientId() {
     	if (clientId!=null) {
     		return true;
@@ -72,14 +115,23 @@ public class AddOauthClient extends FortnoxCommand implements Action {
     	}
     	sess.getConsole().println(message);
     }
-    
-	@Override
-	public Object execute() throws Exception {
 
-		initVariables();
-		printFortnoxClient();
-		
-		return null;
-	}
+    private void createClient() {
+    	
+    	creator = new Fortnox4jCLI();
+    	creator.setClientId(clientId);
+    	creator.setClientSecret(clientSecret);
+    	
+    	creator.setFortnoxClientManager(clientManager, orgNo);
+    	if (creator.hasAuthenticationCode()) {
+    		creator.setGetAccessToken(true);
+    	} else {
+    		creator.setGetAllTokens(true);
+    	}
+    	
+    	creator.determineCommandToRun();
+    	
+    	
+    }
 
 }
