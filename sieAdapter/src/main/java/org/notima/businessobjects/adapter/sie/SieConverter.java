f8c50e75a1d8ce4e3c7fd8c387448c4c0d244020
Daniel Tamm <daniel@notima.se>
2024-11-01 15:01:13 +0100

    Handle costcenters and project when exporting to SIE.

--- sieAdapter/src/main/java/org/notima/businessobjects/adapter/sie/SieConverter.java
+++ sieAdapter/src/main/java/org/notima/businessobjects/adapter/sie/SieConverter.java
@@ -3,18 +3,46 @@ package org.notima.businessobjects.adapter.sie;
 import org.notima.generic.businessobjects.AccountingVoucher;
 import org.notima.generic.businessobjects.AccountingVoucherLine;
 import org.notima.util.LocalDateUtils;
+import org.notima.sie.SIEFile;
 import org.notima.sie.TransRec;
 import org.notima.sie.VerRec;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.notima.sie.ObjRec;
+
 public class SieConverter {
 
+	private SIEFile sieFile;
+	
+	public SieConverter(SIEFile sieFile) {
+		this.sieFile = sieFile;
+	}
+	
+	public List<ObjRec> getDimensions(AccountingVoucher av) {
+		
+		List<ObjRec> result = new ArrayList<ObjRec>();
+		ObjRec project;
+		ObjRec costCenter;
+		if (av.getProjectCode()!=null && av.getProjectCode().trim().length()>0) {
+			project = sieFile.addProject(av.getProjectCode().trim(), null);
+			result.add(project);
+		}
+		if (av.getCostCenter()!=null && av.getCostCenter().trim().length()>0) {
+			costCenter = sieFile.addCostCenter(av.getCostCenter(), null);
+			result.add(costCenter);
+		}
+		return result;
+		
+	}
+	
 	/**
 	 * Convert an accounting voucher to a verrec.
 	 * 
 	 * @param src
 	 * @return
 	 */
-	public static VerRec convert(AccountingVoucher src) {
+	public VerRec convert(AccountingVoucher src) {
 		
 		VerRec dst = new VerRec();
 		dst.setSerie(src.getVoucherSeries());
@@ -31,6 +59,7 @@ public class SieConverter {
 			tr.setBelopp(al.getBalance().doubleValue());
 			tr.setKontoNr(al.getAcctNo());
 			tr.setTransText(al.getDescription());
+			tr.setObjektLista(getDimensions(src));
 			dst.addTransRec(tr);
 		}
 		
