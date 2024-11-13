2698bc8c7107cffa8f9c22f9154d30bc234ea145
Daniel Tamm <daniel@notima.se>
2024-11-13 15:05:37 +0100

    Add command list-task-locks

---
+++ adapterTools/src/main/java/org/notima/businessobjects/adapter/tools/command/ListTasks.java
@@ -0,0 +1,39 @@
+package org.notima.businessobjects.adapter.tools.command;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.api.console.Session;
+import org.notima.businessobjects.adapter.tools.task.TaskLock;
+import org.notima.businessobjects.adapter.tools.task.TaskLockManager;
+
+@Command(scope = "notima", name = "list-task-locks", description = "Lists on-going tasks")
+@Service
+public class ListTasks implements Action {
+
+	@Reference
+	private TaskLockManager taskLockManager;
+	
+	@Reference 
+	Session sess;
+	
+	@Override
+	public Object execute() throws Exception {
+
+		if (taskLockManager==null) {
+			sess.getConsole().println("No tasklock manager found");
+		} else {
+			List<TaskLock> locks = taskLockManager.getLocks();
+			for (TaskLock l : locks) {
+				sess.getConsole().println("Lock " + l.getLockId());
+			}
+		}
+		
+		return null;
+	}
+
+}
