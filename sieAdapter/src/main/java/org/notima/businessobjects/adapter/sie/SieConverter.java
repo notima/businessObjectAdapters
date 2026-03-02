f854f417bf4d7b69314ea24199f8f746ae9910d2
Daniel Tamm <daniel@notima.se>
2026-03-02 17:58:15 +0100

    Enable SPI on SIEAdapter.

--- sieAdapter/src/main/java/org/notima/businessobjects/adapter/sie/SieConverter.java
+++ sieAdapter/src/main/java/org/notima/businessobjects/adapter/sie/SieConverter.java
@@ -2,6 +2,8 @@ package org.notima.businessobjects.adapter.sie;
 
 import org.notima.generic.businessobjects.AccountingVoucher;
 import org.notima.generic.businessobjects.AccountingVoucherLine;
+import org.notima.generic.businessobjects.BusinessPartner;
+import org.notima.generic.ifacebusinessobjects.AccountingVoucherConverter;
 import org.notima.util.LocalDateUtils;
 import org.notima.sie.SIEFile;
 import org.notima.sie.TransRec;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.notima.sie.ObjRec;
 
-public class SieConverter {
+public class SieConverter implements AccountingVoucherConverter<VerRec> {
 
 	private SIEFile sieFile;
 	
@@ -64,6 +66,35 @@ public class SieConverter {
 		}
 		
 		return dst;
+	}
+
+	@Override
+	public String getSystemName() {
+		return SieAdapter.SYSTEM_NAME;
+	}
+
+	@Override
+	public void readSource(VerRec src) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTenant(BusinessPartner tenant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BusinessPartner getTenant() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AccountingVoucher> getAccountingVouchers() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
